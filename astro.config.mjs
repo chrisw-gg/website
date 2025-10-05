ef3d07b41a88f5ab45fa7560783f15f12fcc675d
Chris Williams <git@chrisw.gg>
2025-10-05 03:31:56 -0400

    Initial commit for portfolio website

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import AutoImport from 'astro-auto-import';
+import mdx from '@astrojs/mdx';
+import { glob as tinyglobby } from 'tinyglobby';
+
+const components = await tinyglobby(['src/components/**/*.astro', 'src/assets/**/*.astro'], { cwd: "." });
+const autoLoadedComponents = components.map(item => "./" + item);
+
+console.log("Auto importing components: ");
+console.log(autoLoadedComponents);
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    // Set to true to enable experimental responsive image support with defaults
+    experimentalLayout: true, // TODO: Doesn't work?
+    responsiveStyles: true,
+    // Or, set a specific default layout
+    layout: 'constrained',
+  },
+  integrations: [
+    AutoImport({
+      imports: autoLoadedComponents
+    }),
+    // Make sure the MDX integration is included AFTER astro-auto-import
+    mdx(),
+  ],
+});
